adba97e4879fcff778fe0490c50640349b043d40
Isabeau Bergeron <isabeau.bergeron@zaboweb.com>
2018-01-01 21:46:02 -0500

    Added & configured webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
